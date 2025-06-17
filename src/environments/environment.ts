13a4cede069a59a6db3667327d13450f4851aa07
DusanProsic <dusanprosic72@gmail.com>
2025-06-17 02:58:59 +0200

    Prvi

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIza....",
+        authDomain: "your-project.firebaseapp.com",
+        projectId: "your-project",
+        storageBucket: "your-project.appspot.com",
+        messagingSenderId: "123...",
+        appId: "1:123:web:abc"
+    }
+  };
