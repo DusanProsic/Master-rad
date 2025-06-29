6117e030fa5fc541efde6809cdb6ab7f91a79953
DusanProsic <dusanprosic72@gmail.com>
2025-06-29 06:09:19 +0200

    Dodat Auth Guard za zaštitu dashboard stranice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login.page';
 import { RegisterPage } from './pages/register.page';
 import { DashboardPage } from './pages/dashboard.page';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterPage },
-  { path: 'dashboard', component: DashboardPage}
-
+  { path: 'dashboard', component: DashboardPage, canActivate: [authGuard] }
 ];
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from '@angular/fire/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+
+
+export const authGuard: CanActivateFn = () => {
+  const auth = inject(Auth);
+  const router = inject(Router);
+
+  return new Promise<boolean>((resolve) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        resolve(true); // ✅ Logged in
+      } else {
+        router.navigate(['/login']);
+        resolve(false); // ❌ Not logged in
+      }
+    });
+  });
+};
