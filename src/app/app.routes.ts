a854ae85959959076d7be5bc12ade7ba84982379
DusanProsic <dusanprosic72@gmail.com>
2025-06-29 05:30:07 +0200

    Zapravo implementiran prazan dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login.page';
 import { RegisterPage } from './pages/register.page';
+import { DashboardPage } from './pages/dashboard.page';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'login', component: LoginPage },
-  { path: 'register', component: RegisterPage }
+  { path: 'register', component: RegisterPage },
+  { path: 'dashboard', component: DashboardPage}
+
 ];
