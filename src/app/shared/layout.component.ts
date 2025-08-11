0937f48840ae5c5c5bc9fd861f41803ddd3282ca
DusanProsic <dusanprosic72@gmail.com>
2025-08-11 04:21:51 +0200

    uradjen responsive css

--- src/app/shared/layout.component.ts
+++ src/app/shared/layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { RouterModule } from '@angular/router';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -13,7 +14,27 @@ import { RouterModule } from '@angular/router';
 export class LayoutComponent {
   constructor(private authService: AuthService) {}
 
+  sidebarOpen = false;
   logout() {
     this.authService.logout();
   }
+  toggleSidebar() {
+  this.sidebarOpen = !this.sidebarOpen;
+}
+
+closeSidebar() {
+  this.sidebarOpen = false;
+}
+@HostListener('document:click', ['$event'])
+onClickOutside(event: Event) {
+  if (
+    this.sidebarOpen &&
+    !(
+      (event.target as HTMLElement).closest('.sidebar') ||
+      (event.target as HTMLElement).closest('.hamburger')
+    )
+  ) {
+    this.sidebarOpen = false;
+  }
+}
 }
