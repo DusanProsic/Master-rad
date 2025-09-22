002d6a3c6e3ac5ccb6ad192038391da42d5ac940
DusanProsic <dusanprosic72@gmail.com>
2025-09-22 06:09:05 +0200

    estena angual.json pravila

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,30 +1,30 @@
+// app.config.ts
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-
-import { routes } from './app.routes';
-
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, enableIndexedDbPersistence  } from '@angular/fire/firestore';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { routes } from './app.routes';
+import { enableIndexedDbPersistence, enableMultiTabIndexedDbPersistence } from 'firebase/firestore';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { environment } from '../environments/environment';
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
 
-    // Firebase
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: environment.production,               // or !isDevMode()
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
 
-    // Firestore with offline persistence
     provideFirestore(() => {
       const fs = getFirestore();
-      // Enable IndexedDB persistence (ignore multi-tab error)
-      enableIndexedDbPersistence(fs).catch(() => {});
+      enableMultiTabIndexedDbPersistence(fs).catch(() => {});
       return fs;
     }),
   ],
